f126a30e715b55e329c37590c1be76623e350637
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-05 00:05:14 +0300

    f1

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+// PORT = process.env.PORT || 5000; newcomment 
 app.use(cors());
 app.use(express.json());
 
