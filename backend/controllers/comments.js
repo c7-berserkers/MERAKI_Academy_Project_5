786b5f223e5ee5805c8b3557b917ef8033a60c9e
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-06 21:39:44 +0300

    comment reate

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -1,1 +1,30 @@
 const { pool } = require("../models/db");
+
+const createComment = (req, res) => {
+    // const  posts_id   = req.params.id*1;
+    const { comment , users_id , posts_id} = req.body;
+    const data = [comment , users_id ,posts_id];
+    console.log(data)
+    const query = `INSERT INTO comments (comment,posts_id,users_id) VALUES ($1,$2,$3) RETURNING  *;`;
+    pool.query(query, data)
+        .then((result) => {
+            res.status(201).json({
+                success: true,
+                message: "Comments created successfully",
+                result: result.rows,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                success: false,
+                message: "Server error",
+                err: err,
+            });
+        });
+    }
+
+
+    module.exports = {
+        createComment,
+    };
+
--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const commentRouter = express.Router();
 
+const { createComment } = require('../controllers/comments')
+
+//end point
+
+commentRouter.post("/:id", createComment );
+
+
 module.exports = commentRouter;
