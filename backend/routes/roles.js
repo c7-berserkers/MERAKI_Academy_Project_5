c536b8d8e30793a1186f38817143464929e514b4
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-06 20:44:02 +0300

    role routes

--- backend/routes/roles.js
+++ backend/routes/roles.js
@@ -2,4 +2,15 @@ const express = require("express");
 
 const roleRouter = express.Router();
 
+
+
+//end point
+
+roleRouter.post("", createRole );
+roleRouter.post("/permission", createPermission );
+roleRouter.post("/rolePermission", createRolePermission );
+
+
+
+
 module.exports = roleRouter;
