258ba83db063bbdbb9c4f2d3e00166dcb0bcc4f2
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-09 23:41:07 +0300

    add chat function to add a chat

--- backend/controllers/chatAndMessage.js
+++ backend/controllers/chatAndMessage.js
@@ -1,6 +1,28 @@
 const { Error } = require("mongoose");
 const { pool } = require("../models/db");
 
+const createChat = (req, res) => {
+
+    const {sender_user_id , reserver_user_id} = req.body;
+
+    const data = [sender_user_id , reserver_user_id];
+    const query = `INSERT INTO chats (sender_user_id , reserver_user_id) VALUES ($1 ,$2) RETURNING  *;`;
+    pool.query(query, data)
+        .then((result) => {
+            res.status(201).json({
+                success: true,
+                message: "chat created successfully",
+                result: result.rows,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                success: false,
+                message: "Server error",
+                err: err,
+            });
+        });
+    }
 
     
     module.exports = {
