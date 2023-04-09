89fc79ec72e0af7b7b502d95e7614bc17111f480
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-10 00:37:01 +0300

    add on chat route commits to how test the funchion

--- backend/routes/chatAndMessage.js
+++ backend/routes/chatAndMessage.js
@@ -19,16 +19,43 @@ chatAndMessageRouter.delete("/:id", deleteChat);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = chatAndMessageRouter;
+
+
+/*
+ * Testing Object for createChat:
+
+http://localhost:5000/chats
+
+{
+"sender_user_id" :1,
+"reserver_user_id":2
+}
+*/
+
+/*
+ * Testing Object for createMessage:
+
+http://localhost:5000/chats/Message/:id
+
+{
+"sender_user_id" :1 ,
+"message":"replay from 1"
+}
+*/
+
+
+/*
+ * Testing Object for getAllMessageOnSpecificChat:
+
+http://localhost:5000/chats/allMessage/1
+
+*/
+
+
+/*
+ * Testing Object for getAllMessageOnSpecificChat:
+
+http://localhost:5000/chats/1
+
+*/
