302c2f7373d6824c8e69eac2f69eb2f83dd24ceb
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-04-10 21:50:51 +0300

    fixed bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,20 @@ import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import Register from "./components/Register";
 import Login from "./components/Login";
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <NavBar />
+              <WelcomePage />
+            </>
+          }
+        />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
