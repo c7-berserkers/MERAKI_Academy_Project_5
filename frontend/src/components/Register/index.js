10051e788e2ec41187efd7aeb3c731bf106c55c2
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-10 21:29:32 +0300

    import the file that needed in register and put intial value to the input

--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -1,5 +1,32 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import validator from 'validator';
+import { useState } from "react";
+import React from 'react';
+import axios from 'axios';
 
-export default function index() {
-  return <div>index</div>;
+
+
+export default function Register() {
+
+  useStateTestValue={
+    first_name: undefined ,
+    last_name: undefined,
+    age: undefined,
+    country: undefined,
+    email: undefined,
+    password: undefined,
+    img: undefined,
+    role_id: "1",
+    urlMyPhoto: "",
+  }
+
+
+
+  return (<>
+  <div>Register</div>
+  <div> 
+  </div>
+
+  </>);
 }
