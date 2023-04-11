fe83086077f89513856b22d840c1ddb639f286c6
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-11 22:53:08 +0300

    add function to get user data and it posts

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,18 +1,34 @@
 import React, { useContext, useState, useEffect } from "react";
-
+import axios from 'axios';
 
 
 export default function Profile() {
 
+    const [dataUser, setDataUser] = useState({})
+    const [dataUserPost, setDataUserPost] = useState({})
+
 
     useEffect(() => {
-   
-      });
+        axios.get(`http://localhost:5000/users/${localStorage.getItem("userId")}`,{
+            headers: {
+            'Authorization': `${localStorage.getItem("userId")}`
+            }
+        })
+        .then(function (response) {
+        console.log(response.data)
+        setDataUser(response.data.user)
+        setDataUserPost(response.data.userPosts)
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+    },[]);
     
 
 
 
-  return (
+return (
+
     <div>index</div>
-  )
+)
 }
