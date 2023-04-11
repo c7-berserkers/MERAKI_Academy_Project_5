7802f382eaacb558434f38d599bd317f2f488aea
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-11 23:13:08 +0300

    show my profile img

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,34 +1,52 @@
 import React, { useContext, useState, useEffect } from "react";
 import axios from 'axios';
-
+import "./index.css"
 
 export default function Profile() {
 
-    const [dataUser, setDataUser] = useState({})
-    const [dataUserPost, setDataUserPost] = useState({})
+    const [dataUser, setDataUser] = useState(undefined)
+    const [dataUserPost, setDataUserPost] = useState(undefined)
 
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/users/${localStorage.getItem("userId")}`,{
+        axios.get(`http://localhost:5000/users/${localStorage.getItem("userId")}`, {
             headers: {
-            'Authorization': `${localStorage.getItem("userId")}`
+                'Authorization': `${localStorage.getItem("userId")}`
             }
         })
-        .then(function (response) {
-        console.log(response.data)
-        setDataUser(response.data.user)
-        setDataUserPost(response.data.userPosts)
-        })
-        .catch(function (error) {
-        console.log(error);
-        });
-    },[]);
-    
+            .then(function (response) {
+                console.log(response.data)
+                setDataUser(response.data.user)
+                setDataUserPost(response.data.userPosts)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, []);
 
 
 
-return (
 
-    <div>index</div>
-)
+    return (
+
+        <div>
+
+
+            {dataUser ? <>
+
+                <div className="flex">
+                    <div id="wrapper">
+                        <div id="image_div">
+                            <p className="img_wrapper">
+                                <img className="MyProfileImg" src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?cs=srgb&dl=pexels-mohamed-abdelghaffar-771742.jpg&fm=jpg" alt="no photo found" />
+                                <span><input onClick={() => {}} type="button" value="+" /></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </> : <p>noData</p>}
+
+
+        </div>
+    )
 }
