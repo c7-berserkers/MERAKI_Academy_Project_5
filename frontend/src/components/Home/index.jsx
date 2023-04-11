c468239f50476659de623a94c7d2a62f585d26b3
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-04-11 23:57:19 +0300

    working on the posts

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -15,6 +15,10 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Container from "@mui/material/Container";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../redux/reducers/posts";
+
+// ----------------------------------------------
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -27,99 +31,129 @@ const ExpandMore = styled((props) => {
 }));
 
 export default function Home() {
+  const dispatch = useDispatch();
+
+  const { token, posts } = useSelector((state) => {
+    return {
+      token: state.auth.token,
+      posts: state.posts.posts,
+    };
+  });
   const [expanded, setExpanded] = useState(false);
   const BACKEND = process.env.REACT_APP_BACKEND;
-  console.log(BACKEND);
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
+  // ---------------------------------------
+  const getPosts = async () => {
+    try {
+      const result = await axios.get(`${BACKEND}/posts`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (result.data.success) {
+        dispatch(setPosts(result.data.result));
+      } else throw Error;
+    } catch (error) {
+      if (!error.response.data.success) {
+        return console.log(error.response.data.message);
+      }
+      console.log("Error happened while Get Data, please try again");
+    }
+  };
+
   useEffect(() => {
-    // axios.get();
+    getPosts();
+    console.log(posts);
   }, []);
+
   return (
     <div>
       {" "}
       <div className="new-post">x</div>
       <div className="feed">
         <Container>
-          <Card sx={{}}>
-            <CardHeader
-              avatar={
-                <Avatar
-                  sx={{ bgcolor: red[500] }}
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png"
-                  aria-label="recipe"
-                ></Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              // subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              // height="194"
-              image="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
-              alt="post"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
+          {posts.map((post) => {
+            return (
+              <Card key={post.id} style={{ margin: "10px 0" }}>
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      sx={{ bgcolor: red[500] }}
+                      src={post.user_img}
+                      aria-label="recipe"
+                    ></Avatar>
+                  }
+                  action={
+                    <IconButton aria-label="settings">
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title={post.user_first_name}
+                  // subheader="September 14, 2016"
+                />
+                <CardMedia
+                  component="img"
+                  // height="194"
+                  image={post.img}
+                  alt="post"
+                />
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    {post.description}
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
 
-              <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                  skillet over medium-high heat. Add chicken, shrimp and
-                  chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
-                  set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant,
-                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
-                  chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with
-                  artichokes and peppers, and cook without stirring, until most
-                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down
-                  into the rice, and cook again without stirring, until mussels
-                  have opened and rice is just tender, 5 to 7 minutes more.
-                  (Discard any mussels that don&apos;t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
+                  <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                  >
+                    <ExpandMoreIcon />
+                  </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                  <CardContent>
+                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>
+                      Heat 1/2 cup of the broth in a pot until simmering, add
+                      saffron and set aside for 10 minutes.
+                    </Typography>
+                    <Typography paragraph>
+                      Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                      skillet over medium-high heat. Add chicken, shrimp and
+                      chorizo, and cook, stirring occasionally until lightly
+                      browned, 6 to 8 minutes. Transfer shrimp to a large plate
+                      and set aside, leaving chicken and chorizo in the pan. Add
+                      pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                      pepper, and cook, stirring often until thickened and
+                      fragrant, about 10 minutes. Add saffron broth and
+                      remaining 4 1/2 cups chicken broth; bring to a boil.
+                    </Typography>
+                    <Typography paragraph>
+                      Add rice and stir very gently to distribute. Top with
+                      artichokes and peppers, and cook without stirring, until
+                      most of the liquid is absorbed, 15 to 18 minutes. Reduce
+                      heat to medium-low, add reserved shrimp and mussels,
+                      tucking them down into the rice, and cook again without
+                      stirring, until mussels have opened and rice is just
+                      tender, 5 to 7 minutes more. (Discard any mussels that
+                      don&apos;t open.)
+                    </Typography>
+                    <Typography>
+                      Set aside off of the heat to let rest for 10 minutes, and
+                      then serve.
+                    </Typography>
+                  </CardContent>
+                </Collapse>
+              </Card>
+            );
+          })}
         </Container>
       </div>
     </div>
