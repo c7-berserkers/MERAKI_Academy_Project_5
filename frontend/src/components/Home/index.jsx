260d8be8f4779b60c0ec7d487b937f86e9e824bf
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-04-12 21:13:57 +0300

    show more button fix

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -43,8 +43,14 @@ export default function Home() {
   });
   const [expanded, setExpanded] = useState(false);
   const BACKEND = process.env.REACT_APP_BACKEND;
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const handleExpandClick = (str) => {
+    return () => {
+      if (expanded) {
+        setExpanded(false);
+      } else {
+        setExpanded(str);
+      }
+    };
   };
 
   // ---------------------------------------
@@ -112,45 +118,23 @@ export default function Home() {
                   </IconButton>
 
                   <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
+                    expand={expanded === post.id}
+                    onClick={handleExpandClick(post.id)}
+                    aria-expanded={expanded === post.id}
                     aria-label="show more"
                   >
                     <MdComment />
                   </ExpandMore>
                 </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <Collapse
+                  in={expanded === post.id}
+                  timeout="auto"
+                  unmountOnExit
+                >
                   <CardContent>
-                    <Typography paragraph>Method:</Typography>
                     <Typography paragraph>
                       Heat 1/2 cup of the broth in a pot until simmering, add
                       saffron and set aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                      Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                      skillet over medium-high heat. Add chicken, shrimp and
-                      chorizo, and cook, stirring occasionally until lightly
-                      browned, 6 to 8 minutes. Transfer shrimp to a large plate
-                      and set aside, leaving chicken and chorizo in the pan. Add
-                      pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                      pepper, and cook, stirring often until thickened and
-                      fragrant, about 10 minutes. Add saffron broth and
-                      remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography paragraph>
-                      Add rice and stir very gently to distribute. Top with
-                      artichokes and peppers, and cook without stirring, until
-                      most of the liquid is absorbed, 15 to 18 minutes. Reduce
-                      heat to medium-low, add reserved shrimp and mussels,
-                      tucking them down into the rice, and cook again without
-                      stirring, until mussels have opened and rice is just
-                      tender, 5 to 7 minutes more. (Discard any mussels that
-                      don&apos;t open.)
-                    </Typography>
-                    <Typography>
-                      Set aside off of the heat to let rest for 10 minutes, and
-                      then serve.
                     </Typography>
                   </CardContent>
                 </Collapse>
