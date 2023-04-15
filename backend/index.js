8bed0d228ecc154595e3dfdaa352ce97ca1b639a
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-04-16 02:51:04 +0300

    group chats backend saves chats

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const socket = require("socket.io");
+const { Server } = require("socket.io");
 require("dotenv").config();
 require("./models/db");
 const chatDB = require("./models/chatdb");
+const chatModel = require("./models/chats");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -34,28 +35,34 @@ const server = app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: `http://localhost:${PORT}`,
-//     methods: [GET, POST],
-//   },
-// });
-// io.on(connection, (socket) => {
-//   console.log(rooms, socket.rooms);
-//   socket.on(JOIN_ROOM, (data) => {
-//     console.log(data, data);
-//     socket.join(data);
-//     console.log(rooms, socket.rooms);
-//   });
-//   socket.on(SEND_MESSAGE, async (data) => {
-//     console.log(data, data);
-//     const content = { sender: data.sender, message: data.message };
-//     const roomId = data.roomId;
-//     // save the message here
-//     socket.to(roomId).emit(RECEIVE_MESSAGE, content);
-//   });
+const io = new Server(server, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log("rooms", socket.rooms);
+  socket.on("JOIN_ROOM", (data) => {
+    console.log("data", data);
+    socket.join(data);
+    console.log("rooms", socket.rooms);
+  });
+  socket.on("SEND_MESSAGE", async (data) => {
+    console.log("data", data);
+    const content = { sender: data.sender, message: data.message };
+    const roomId = data.roomId;
 
-//   socket.on(disconnect, () => {
-//     console.log("User Disconnected", socket.id);
-//   });
-// });
+    // save the message here
+    chatModel.findOneAndUpdate(
+      { chat_name: roomId },
+      { $push: { messages: content } },
+      { new: true }
+    );
+    socket.to(roomId).emit(RECEIVE_MESSAGE, content);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
