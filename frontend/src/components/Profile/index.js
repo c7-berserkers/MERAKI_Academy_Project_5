f56e2f285308b413c37c687dcfd96f86500b8894
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-18 23:39:25 +0300

    impotant change in profile page **

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -188,7 +188,7 @@ export default function Profile() {
       })
       .catch(function (error) {
         console.log(error);
-        // navigate("/NotFound");
+        navigate("/NotFound");
       });
 
 
