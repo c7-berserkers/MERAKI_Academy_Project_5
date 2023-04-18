a6eecb799f2e3431e0365046399b2a627fc44a6b
AhmadTIsmail <ahmad.thaer.ismail@gmail.com>
2023-04-18 23:38:16 +0300

    impotant change in profile page

--- frontend/src/components/Dashboard/UsersChange/index.js
+++ frontend/src/components/Dashboard/UsersChange/index.js
@@ -1,13 +1,15 @@
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 
 function Users() {
     //===============================================================
     let token = localStorage.getItem("token");
     //===============================================================
+    const [allUser, setAllUser] = useState(false)
 
     useEffect(() => {
         axios
@@ -17,8 +19,9 @@ function Users() {
                 },
             })
             .then(function (response) {
-                
-                console.log(response.data.users,"users")
+
+                console.log(response.data.users, "users")
+                setAllUser(response.data.users)
             })
             .catch(function (error) {
                 console.log(error);
@@ -29,33 +32,48 @@ function Users() {
     }, []);
 
 
-    const deleteUser=()=>{}
-    const unDeleteUser=()=>{}
-    const Admin=()=>{}
-    const NotAdmin=()=>{}
+    const deleteUser = () => { }
+    const unDeleteUser = () => { }
+    const Admin = () => { }
+    const NotAdmin = () => { }
 
 
 
     return (<>
-            <ListGroup.Item action >
-                All User
-            </ListGroup.Item>
+        <ListGroup.Item action >
+            All User
+        </ListGroup.Item>
         <ListGroup as="ol" numbered>
-            <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-            >
-                <div className="ms-2 me-auto">
-                    <div className="fw-bold">Subheading</div>
-                    Cras justo odio
-                </div>
-                <Badge bg="primary" pill>
-                    14
-                </Badge>
-            </ListGroup.Item>
-            
+            {allUser ? <>
+
+                {allUser.map((user) => {
+                    return (
+                        <ListGroup.Item 
+                            key={user.id}
+                            as="li"
+                            className="d-flex justify-content-between align-items-start"
+                        >
+                            <div className="ms-2 me-auto">
+                                <div className="fw-bold">{user.first_name} {user.last_name}</div>
+                                {/* <Avatar alt="Remy Sharp" src={user.img} /> */}
+                                Cras justo odio
+                            </div>
+                            <Badge bg="primary" pill>
+                                14
+                            </Badge>
+                        </ListGroup.Item>
+
+                    )
+                })}
+
+                { }
+
+            </> : <></>}
+
+
+
         </ListGroup>
-        </>
+    </>
     )
 }
 
