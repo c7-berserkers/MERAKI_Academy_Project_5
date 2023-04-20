a25ab9c92fcc0215677df356ce30138df6f4eb61
OsamaGhydah <osama.ghydah@gmail.com>
2023-04-20 22:04:32 +0300

    create users page

--- frontend/src/components/Dashboard/UsersChange/index.js
+++ frontend/src/components/Dashboard/UsersChange/index.js
@@ -14,7 +14,7 @@ function Users() {
     const [allUser, setAllUser] = useState(false)
     const [toggle, setToggle] = useState(true)
 
-    useEffect(() => {
+    const getAllUsers = () => {
         axios
             .get(`${process.env.REACT_APP_BACKEND}/users/`, {
                 headers: {
@@ -22,17 +22,21 @@ function Users() {
                 },
             })
             .then(function (response) {
-
                 console.log(response.data.users, "users")
                 setAllUser(response.data.users)
             })
             .catch(function (error) {
                 console.log(error);
             });
-        //===============================================================
+        }
+    //===============================================================
+
+    useEffect(() => {getAllUsers()},[toggle]);
+
+    //===============================================================
 
 
-    }, [toggle]);
+    
 
 
     const deleteUser = (id) => {
@@ -60,10 +64,66 @@ function Users() {
     }
 
 
-    const Admin = () => { }
-    const NotAdmin = () => { }
+    const Admin = (e) => {
+console.log(e.targrt.value)
+     }
+    const NotAdmin = (e) => {
+        console.log(e.targrt.value)
+
+     }
+    const disband = (e) => {
+        console.log(e.targrt.value)
+
+     }
+    const band = (e) => {
+        console.log(e.targrt.value)
+
+     }
 
 
+    const allUserFunction=()=>{
+        return allUser.length>0?allUser.map((user) => {
+            return (
+                <ListGroup.Item
+                    key={user.id}
+                    as="li"
+                    className="d-flex justify-content-between align-items-start">
+                    <div className="ms-3 me-auto">
+                        <Stack spacing={2} direction="row">
+                            <Avatar alt="Remy Sharp" src={user.img} />
+                            <Stack spacing={2} direction="column">
+                                <div>{user.first_name} {user.last_name}</div>
+                                <div>{user.created_at?.split("T")[0]}</div>
+                            </Stack>
+                        </Stack>
+                    </div>
+                    <div >
+                        <Stack spacing={2} direction="column">
+
+                            {user.role_id==1 ? <>
+                            <Button variant="outlined" value={user.id} onClick={(e)=>{
+                                NotAdmin(e)
+                            }}>unAdmin</Button>
+                            </> : <>
+                            <Button variant="outlined"  value={user.id} onClick={(e)=>{
+                                Admin(e)
+                            }}>admin</Button></>}
+
+                            {user.is_deleted ? <>
+                            <Button variant="outlined"  value={user.id} onClick={(e)=>{
+                                disband(e)
+                            }}>disband</Button>
+                            </> : <>
+                            <Button variant="outlined"  value={user.id} onClick={(e)=>{
+                                band(e)
+                            }}>band</Button></>}
+
+                        </Stack>
+                    </div>
+                </ListGroup.Item>
+            )
+                            }):<p>no user yet</p>
+                        }
 
     return (
         <Container>
@@ -71,52 +131,7 @@ function Users() {
                 All User
             </ListGroup.Item>
             <ListGroup as="ol" numbered>
-                {allUser ? <>
-
-
-
-                    {allUser.map((user) => {
-                        return (
-                            <ListGroup.Item
-                                key={user.id}
-                                as="li"
-                                className="d-flex justify-content-between align-items-start">
-                                <div className="ms-3 me-auto">
-                                    <Stack spacing={2} direction="row">
-                                        <Avatar alt="Remy Sharp" src={user.img} />
-                                        <Stack spacing={2} direction="column">
-                                            <div>{user.first_name} {user.last_name}</div>
-                                            <div>{user.created_at?.split("T")[0]}</div>
-                                        </Stack>
-                                    </Stack>
-                                </div>
-                                <div >
-                                    <Stack spacing={2} direction="column">
-
-
-
-
-                                        {user.is_deleted ? <>
-                                        <Button variant="outlined" id={user.id} onClick={(e)=>{unDeleteUser(e.target.id)}} >unDelete</Button>
-                                        </> : <>
-                                        <Button variant="outlined" id={user.id} onClick={(e)=>{deleteUser(e.target.id)}} >delete</Button></>}
-                                        
-
-
-
-                                        {user.is_deleted ? <>
-                                        <Button variant="outlined">unAdmin</Button>
-                                        </> : <>
-                                        <Button variant="outlined">admin</Button></>}
-
-
-                                    </Stack>
-                                </div>
-                            </ListGroup.Item>
-                        )
-                    })}
-                    { }
-                </> : <></>}
+                {allUserFunction()}
             </ListGroup>
         </Container>
     )
