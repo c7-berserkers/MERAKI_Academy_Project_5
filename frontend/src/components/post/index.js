8cd77dc012e1362ed8f974b81ef7e87424c97d30
OsamaGhydah <osama.ghydah@gmail.com>
2023-04-28 16:12:05 +0300

    update 2

--- frontend/src/components/post/index.js
+++ frontend/src/components/post/index.js
@@ -150,7 +150,6 @@ const Post = () => {
       return e.posts_id === id;
     });
   };
-
   //===============================================================
   const deletePost = async (id) => {
     try {
@@ -307,66 +306,67 @@ const Post = () => {
               </p>
             </CardContent>
             <CardActions disableSpacing>
-              {isLiked(likes, post[0].id) !== -1 ? (
-                <div style={{ display: "flex" }}>
-                  <IconButton
-                    onClick={(e) => {
-                      axios
-                        .delete(`${BACKEND}/likes/${post[0].id}`, {
-                          headers: {
-                            Authorization: `Bearer ${token}`,
-                          },
-                        })
-                        .then((result) => {
-                          dispatch(removeLikePost(post[0].id));
-                          dispatch(removeLike(post[0].id));
-                        })
-                        .catch((err) => {
-                          console.log(err);
-                        });
-                    }}
-                    aria-label="add to favorites"
-                  >
-                    <FavoriteIcon style={{ color: "red" }} />
-                  </IconButton>
-                  <p style={{ margin: "10px" }}>
-                    {parseInt(post[0].likes_count)}
-                  </p>
-                </div>
-              ) : (
-                <div style={{ display: "flex" }}>
-                  {" "}
-                  <IconButton
-                    onClick={(e) => {
-                      const payload = {
-                        posts_id: post[0].id,
-                        user_id: userId,
-                      };
-                      axios
-                        .post(
-                          `${BACKEND}/likes/${post[0].id}`,
-                          {},
-                          {
-                            headers: {
-                              Authorization: `Bearer ${token}`,
-                            },
-                          }
-                        )
-                        .then((result) => {
-                          dispatch(addLikePost(post[0].id));
-                          dispatch(addLike(payload));
-                        })
-                        .catch((err) => {
-                          console.log(err);
-                        });
-                    }}
-                    aria-label="add to favorites"
-                  >
-                    <FavoriteIcon />
-                  </IconButton>
-                  <p style={{ margin: "10px" }}>{post[0].likes_count}</p>
-                </div>
-              )}
+            {isLiked(likes, post[0].id) !== -1 ? (
+                        <div style={{ display: "flex" }}>
+                          <IconButton
+                            onClick={(e) => {
+                              axios
+                                .delete(`${BACKEND}/likes/${post[0].id}`, {
+                                  headers: {
+                                    Authorization: `Bearer ${token}`,
+                                  },
+                                })
+                                .then((result) => {
+                                  dispatch(removeLikePost(post[0].id));
+                                  dispatch(removeLike(post[0].id));
+                                })
+                                .catch((err) => {
+                                  console.log(err);
+                                });
+                            }}
+                            aria-label="add to favorites"
+                          >
+                            <FavoriteIcon style={{ color: "red" }} />
+                          </IconButton>
+                          <p style={{ margin: "10px" }}>
+                            {parseInt(post[0].likes_count)}
+                          </p>
+                        </div>
+                      ) : (
+                        <div style={{ display: "flex" }}>
+                          {" "}
+                          <IconButton
+                            onClick={(e) => {
+                              const payload = {
+                                posts_id: post[0].id,
+                                user_id: userId,
+                              };
+                              axios
+                                .post(
+                                  `${BACKEND}/likes/${post[0].id}`,
+                                  {},
+                                  {
+                                    headers: {
+                                      Authorization: `Bearer ${token}`,
+                                    },
+                                  }
+                                )
+                                .then((result) => {
+                                  dispatch(addLikePost(post[0].id));
+                                  dispatch(addLike(payload));
+                                })
+                                .catch((err) => {
+                                  console.log(err);
+                                });
+                            }}
+                            aria-label="add to favorites"
+                          >
+                            <FavoriteIcon />
+                          </IconButton>
+                          {console.log(post.likes_count)}
+                          <p style={{ margin: "10px" }}>{post.likes_count}</p>
+                        </div>
+                      )}
 
               <ExpandMore
                 expand={expanded === post[0].id}
